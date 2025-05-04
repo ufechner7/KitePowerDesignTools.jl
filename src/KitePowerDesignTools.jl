2036df8cd825f150f827b07e09cc3372f81a9535
Uwe Fechner <fechner@aenarete.eu>
2025-05-04 11:46:45 +0200

    Add function se()

--- src/KitePowerDesignTools.jl
+++ src/KitePowerDesignTools.jl
@@ -1,5 +1,13 @@
 module KitePowerDesignTools
 
-# Write your package code here.
+using YAML
+
+export se
+
+SETTINGS::String = "data/settings.yaml"
+
+function se()
+    YAML.load_file(SETTINGS)
+end
 
 end
