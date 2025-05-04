3dc00924b1afb61967b80ac3f1ddf22f31f521cd
Uwe Fechner <fechner@aenarete.eu>
2025-05-04 12:05:35 +0200

    The function se() returns a struct now

--- src/KitePowerDesignTools.jl
+++ src/KitePowerDesignTools.jl
@@ -1,13 +1,26 @@
 module KitePowerDesignTools
 
-using YAML
+using YAML, StructTypes
 
 export se
 
 SETTINGS::String = "data/settings.yaml"
 
+mutable struct DTSettings
+    radius
+    area
+    aspect_ratio
+    DTSettings() = new()
+end
+
+StructTypes.StructType(::Type{DTSettings}) = StructTypes.Mutable()
+
 function se()
-    YAML.load_file(SETTINGS)
+    dts = DTSettings()
+    dict = YAML.load_file(SETTINGS)
+    dict1 = Dict(Symbol(k) => v for (k, v) in dict["carousel"])
+    dict2 = Dict(Symbol(k) => v for (k, v) in dict["kite"])
+    StructTypes.constructfrom!(dts, merge(dict1, dict2))
 end
 
 end
